7905792d4d0896fd1362871ee2afc38135ffb47d
Kolyabis <2z.dizayn@gmail.com>
2014-12-19 22:29:48 +0200

    test

---
+++ www/application/views/index.php
@@ -0,0 +1,18 @@
+<?php //; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Система інтерактивного діалогу</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link href="/css/style.css" rel="stylesheet" type="text/css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="/script.js"></script>
+</head>
+<body>
+    <div>
+
+    </div>
+</body>
+</html>
