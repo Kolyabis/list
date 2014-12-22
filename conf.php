220cd12ab33a628df467f94c72251200ff3fe066
Kolyabis <2z.dizayn@gmail.com>
2014-12-22 09:50:52 +0200

    22.12.2014

--- conf.php
+++ conf.php
@@ -5,7 +5,28 @@ set_include_path(get_include_path()
     .PATH_SEPARATOR.'application/controllers'
     .PATH_SEPARATOR.'application/models'
     .PATH_SEPARATOR.'application/views');
-function __autoload($class){
-    require_once $class.'.php';
+function core($class){
+    include $class.'.php';
 }
-
+spl_autoload_register('core');
+class Db_ext {
+    protected static $dsn = 'mysql:dbname=sid2;host=localhost';
+    protected static $user = 'root';
+    protected static $password = '';
+    protected static $_instance;
+    public static function getInstance(){
+        if(self::$_instance instanceof PDO){
+            return self::$_instance;
+        }else{
+            try{
+                return self::$_instance = new PDO(self::$dsn,self::$user,self::$password);
+            }catch(Exception $error){
+                echo $error->getCode().'<br>';
+                echo $error->getMessage().'<br>';
+                echo $error->getLine().'<br>';
+            }
+        }
+    }
+    protected function __clone(){}
+}
+$db = Db_ext::getInstance();
