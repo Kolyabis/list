6449672f2fdf604c9e09b57230b6f7b03173271e
Kolyabis <2z.dizayn@gmail.com>
2014-12-23 14:15:05 +0200

    23.12.14

--- modules/mod_menu.php
+++ modules/mod_menu.php
@@ -1,21 +1,56 @@
 <?php
 class mod_menu{
-    /* mainMenu */
-    public function mainMenu($db){
-        $rez = $db->query("select * from mainmenu");
+    /* Get_Menu */
+    public function get_menu(){
+        $db = mysql_connect('localhost','root','');
+        if(!$db){
+            exit(mysql_error());
+        }
+        if(!mysql_select_db('2z',$db)){
+            exit(mysql_error());
+        }
+        $sql = "select id, page, parent_id from mainmenu";
+        $result = mysql_query($sql);
+        if(!$result){
+            return null;
+        }
         $mainMenu = array();
-        $rez = $rez->fetchAll(PDO::FETCH_ASSOC);
-        $cnt = count($rez)-1;
-        for($i = 0; $i <= $cnt; $i++){
-            if(empty($rez[$i]['child'])){
-                $mainMenu[$i] = $rez[$i];
-                for($n = 0; $n <= $cnt; $n++){
-                    if($rez[$n]['child'] == $rez[$i]['id']){
-                        $mainMenu[$i]['child'][] = $rez[$n];
-                    }
+        if(mysql_num_rows($result) != 0){
+            for($i = 0; $i < mysql_num_rows($result); $i++){
+                $row = mysql_fetch_array($result, MYSQL_ASSOC);
+                if(empty($mainMenu[$row['parent_id']])){
+                    $mainMenu[$row['parent_id']] = array();
                 }
+                $mainMenu[$row['parent_id']][] = $row;
             }
         }
-        return $mainMenu;
+        echo '<pre>';
+        print_r($mainMenu);
+        /*$sqlCnt = $db->query("select count(id) from mainmenu where parent_id > 0");
+        $cnt = $sqlCnt->fetchColumn();
+        $sql = $db->query("select id, page, parent_id from mainmenu");
+        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+        if(!$result){
+            return null;
+        }
+        $mainMenu = array();
+        $row = mysql_num_rows($db->query("select id, page, parent_id from mainmenu"));
+        print_r($row);
+        if($cnt != 0){
+            echo 'Ne 0 ';
+            for($i = 0; $i < $cnt; $i++){
+                if(empty($mainMenu[$result[$i]['parent_id']])){
+                    //$mainMenu[$result['parent_id']] = array();
+                    echo $result[$i]['parent_id'];
+                }
+                $mainMenu[$result[$i]['parent_id']] = $result;
+
+            }
+
+        }
+        echo ' '.$cnt;
+        echo '<pre>';
+        print_r($mainMenu);
+        echo '</pre>';*/
     }
 }
