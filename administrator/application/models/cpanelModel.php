0b8f50b412891ade912daab77e8ec3c2491b5950
Kolyabis <2z.dizayn@gmail.com>
2014-12-29 08:47:19 +0200

    29.12.14

---
+++ administrator/application/models/cpanelModel.php
@@ -0,0 +1,16 @@
+<?php
+class CpanelModel{
+    public function cpanel($file,$params = null){
+        $db = Db::getInstance();
+        $lib = new lib();
+        $lib->get_session();
+        $params['login'] = $lib->get_user($params['token'], $db);
+        $params['last_modifay'] = $lib->info_modifay($db);
+        /* Присоединяю массив меню панели */
+        $params['menuPanel'] = $lib->menuPanel();;
+        $lib->checkArray($params);
+        ob_start();
+        include(__DIR__.'/'.$file);
+        return ob_get_clean();
+    }
+}
