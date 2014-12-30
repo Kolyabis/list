286295969c6e8175a642fe0d092de05e487adc5a
Kolyabis <2z.dizayn@gmail.com>
2014-12-30 14:05:53 +0200

    30.12.14

--- administrator/application/lang/langueg.php
+++ administrator/application/lang/langueg.php
@@ -2,3 +2,16 @@
 $category = 'Категории';
 $mainmenu = 'Меню';
 $user = 'Пользователь';
+$id = 'Поле';
+$name = 'Наименование';
+$description = 'Описание';
+$parent_id = 'Родитель';
+$data = 'Дата';
+$page = 'Страница';
+$controller = 'Сылка';
+$mail = 'Mail';
+$role = 'Уровень доступа';
+$token = 'Ключ';
+$pass = 'Пароль';
+$tel = 'Тел';
+$status = 'Статус';
