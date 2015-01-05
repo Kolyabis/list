61863e4c191cd7ea999500394548d0779ff762f8
Kolyabis <2z.dizayn@gmail.com>
2015-01-05 16:39:51 +0200

    05.01.15

--- administrator/application/controllers/ajaxController.php
+++ administrator/application/controllers/ajaxController.php
@@ -11,4 +11,12 @@ class ajaxController implements IController{
     public function updateAction(){}
     public function insertAction(){}
     public function deleteAction(){}
+    public function deleteCategory(){
+        $fc = FrontController::getInstance();
+        $params = $fc->getParams();
+        $view = new ajaxModel();
+        $view->params = $params;
+        $result = $view->ajax('../views/ajax.php', $params);
+        $fc->setBody($result);
+    }
 }
